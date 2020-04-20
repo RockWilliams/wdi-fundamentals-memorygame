d75a2227660a3bc355e77b308dc2725e15cb8d62
Rock <rock7699@gmail.com>
2020-04-20 03:32:35 -0700

    cards now flip

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -25,6 +25,7 @@ const cards = [
 let cardsInPlay = [];
 
 function checkForMatch(){
+
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]) {
 			alert('You found a match!');
@@ -34,13 +35,27 @@ function checkForMatch(){
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+createBoard = function() {
+
+	for(let i = 0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+
+}
+
+createBoard();
